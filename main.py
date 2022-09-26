20bc05ee4d597cf35bd8071f9b78864ac8a41263
Anfal <ahassan2@mof.gov.kw>
2022-09-26 19:22:50 +0300

    finish task 1 of week 2 day 2

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+#AnfalAlali
 # Create a dictionary called `person`, it has three properties, `name` which is a string, `age` which is an integer, `hobbies` which is a list of strings.
-
+person{
+    "name":"Anfal",
+     "age": 29,
+     "hobbies":["running","swimming"]
+}
 
 # Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
 
 
 def change_age(person, number):
-    ...
-
-
+    person["age"]=number
+    return person
 print(change_age(person, 21))
 
 
@@ -15,19 +19,17 @@ print(change_age(person, 21))
 
 
 def add_hobby(person, hobby):
-    ...
-
-
-print(add_hobby(person, "Table Tennis"))
-
+    person["hobbies"].append(hobby)
+    return person
+print(add_hobby(person,"table Tenneis"))
+  
 
 # CHALLENGE
 
 # Create a function called `add_job` which takes `person` dictionary and `job` which is a string, as arguments. This function checks, if the dictionary does not have a property called `job` then it will add it to the dictionary. The function will return the updated dictionary.
 
 
-def add_job(person, job):
-    ...
+        
 
 
 # 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. This function checks the number of hobbies for this person, if it's more than three hobbies, print to the user a message telling them that they're talented.
